01b81f25ade588bb4b8c807eb56d38000446f681
RobertZap <zapalowicz.robert@gmail.com>
2023-07-04 16:56:26 +0200

    product item schema

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 function Products() {
     const [products, setProducts] = useState([]);
@@ -37,6 +38,7 @@ function Products() {
                     <h2>{product.name}</h2>
                     <p>{product.category}</p>
                     <p>{product.price}</p>
+                    <Link to={`/product/${product.id}`}>Details</Link>
                 </div>
             ))}
         </>
