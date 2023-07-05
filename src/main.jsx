08375b56770fe4b03139693ee45ce0e8e9388466
RobertZap <zapalowicz.robert@gmail.com>
2023-07-05 16:58:58 +0200

    mui for material design

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import router from "./router.jsx";
 import { RouterProvider } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
+import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
+        <CssBaseline />
         <RouterProvider router={router} />
     </React.StrictMode>
 );
