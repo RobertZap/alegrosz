71b96f6d832eb7d134f4f734bc630ba7fe3111cc
RobertZap <zapalowicz.robert@gmail.com>
2023-07-05 12:58:12 +0200

    add product, delete product, redirect with useNavigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Products from "./components/Products.jsx";
+import { Link } from "react-router-dom";
 
 function App() {
     return (
         <>
+            <Link to="add-product">Add product</Link>
             <Products />
         </>
     );
